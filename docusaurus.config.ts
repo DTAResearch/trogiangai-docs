d2d474d3dae2da689686a070356efb815d852f09
tran-the-lam <lamtt2403@gmail.com>
2025-06-02 15:59:18 +0700

    update social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -74,7 +74,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/logo.png',
     navbar: {
       title: 'Trợ giảng AI',
       logo: {
