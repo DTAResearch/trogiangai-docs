cc6d3aa712fe45a216d575c88b012f0436785d63
tran-the-lam <lamtt2403@gmail.com>
2025-09-23 09:27:05 +0700

    update url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,10 +18,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://dtaresearch.github.io',
+  url: 'https://hdsd.trogiang.hoctiep.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/trogiangai-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
